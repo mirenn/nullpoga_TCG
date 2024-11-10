60c5c66585a9404bd5da449c611c68194a4e3b0f
mirenn <ngihyt910@outlook.jp>
2024-11-10 21:46:44 +0900

    ボタンコンテナのボタンを整形

--- pythonProject/nullpoga-frontend/src/components/ButtonContainer.tsx
+++ pythonProject/nullpoga-frontend/src/components/ButtonContainer.tsx
@@ -14,11 +14,31 @@ const ButtonContainer = ({
   onSpellPhaseEnd,
 }: ButtonContainerProps) => (
   <div className="button-container">
-    <button id="get-game-state" className="blue-button" onClick={onGetGameState}>Get Game State</button>
+    <button
+      id="get-game-state"
+      className="blue-button"
+      onClick={onGetGameState}
+    >
+      Get Game State
+    </button>
     {/* <button id="render-hand" className="blue-button" onClick={onRenderHand}>Render Hand</button> */}
-    <button id="action-submit" className="blue-button" onClick={onActionSubmit}>Submit Actions</button>
-    <button id="summon-phase-end" className="blue-button" onClick={onSummonPhaseEnd}>End Summon Phase</button>
-    <button id="spell-phase-end" className="blue-button" onClick={onSpellPhaseEnd}>End Spell Phase</button>
+    <button
+      id="summon-phase-end"
+      className="blue-button"
+      onClick={onSummonPhaseEnd}
+    >
+      End Summon Phase
+    </button>
+    <button
+      id="spell-phase-end"
+      className="blue-button"
+      onClick={onSpellPhaseEnd}
+    >
+      End Spell Phase
+    </button>
+    <button id="action-submit" className="blue-button" onClick={onActionSubmit}>
+      Submit Actions
+    </button>
   </div>
 );
 
