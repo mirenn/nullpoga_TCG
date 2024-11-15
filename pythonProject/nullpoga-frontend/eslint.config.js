38e7026e167b3ec0a10e50c2dd8bbd4d88fdeefe
mirenn <ngihyt910@outlook.jp>
2024-11-16 07:12:39 +0900

    viteのreadmeに従いeslint.config.js修正

--- pythonProject/nullpoga-frontend/eslint.config.js
+++ pythonProject/nullpoga-frontend/eslint.config.js
@@ -3,26 +3,33 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      project:[ 'tsconfig.node.json', './tsconfig.app.json' ],
+      tsconfigRootDir: import.meta.dirname,
     },
+    settings: { react: { version: '18.3' } },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+      // Set the react version
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
     },
   },
 )
